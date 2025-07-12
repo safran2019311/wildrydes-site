1152e9612c6d74cba4d6dc43e0c6d2884bb01e90
safran2019311 <safran.2019311@iit.ac.lk>
2025-07-12 20:36:07 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'eu-north-1_bn03ToFPj', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '5t7k6g6hc2c7nk9rmjof2bei5r', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-north-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
